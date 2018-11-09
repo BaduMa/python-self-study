110747cd4e2c463eefb33e1591c9ffbaff2ff5c3
BaduMa <44886705+BaduMa@users.noreply.github.com>
2018-11-09 12:00:59 +0800

    Add files via upload

---
+++ print.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/11/9 11:57
+# @Author : maxu
+
+
+
+# print练习：
+print('hello world!')
+print('100+200=',100+200)
+print(10/3) #10除以3
+print(10%3) #10除以3取余数
+print(10%-4) #10除以-4取余数
+print(2*3) #2乘以3
+print(2**3) #2的三次幂
+print(pow(2,3)) #2的3次幂
+print(-3**2) #3的二次幂的负数
+print((-3)**2) #（负数3）的二次幂
