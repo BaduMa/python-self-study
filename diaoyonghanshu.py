3fa909ce238796530a7c8be7d4d81d19bec777d6
BaduMa <44886705+BaduMa@users.noreply.github.com>
2018-11-09 11:42:02 +0800

    Add files via upload

---
+++ diaoyonghanshu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/11/9 11:41
+# @Author : maxu
+
+
+# 调用函数
+print(pow(2,3)) #2的3次幂
+print(abs(-10)) #绝对值
+print(max(1,3,-4,100,9))
+n1 =255
+n2 =1000
+print(hex(n1))
+print(hex(n2))
+
+# 数据类型转换
+print(int('123'))
+print(int(12.4))
+print(float('12.45'))
+print(str('12.3333'))
+print(bool(1))
+print(bool(''))
