3c998b42f0d020cb9c5373c243906299ba893f9d
BaduMa <44886705+BaduMa@users.noreply.github.com>
2018-11-15 11:03:57 +0800

    Add files via upload

---
+++ diedaiqi.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/11/13 14:47
+# @Author : maxu
+
+# 直接作用于for循环的数据类型有以下几种：
+# 一类是集合数据类型，如list、tuple、dict、set、str等；
+# 一类是generator，包括生成器和带yield的generator function。
+
+# 这些可以直接作用于for循环的对象统称为可迭代对象：Iterable  根据boolean值看是否可以迭代
+from collections.abc import Iterable
+print(isinstance([],Iterable))  #list
+print(isinstance({},Iterable))  #dict
+print(isinstance('abcd',Iterable))
+print(isinstance((x for x in range(10)),Iterable))  #列表生成式
+print(isinstance(12345,Iterable))
+
+# 而生成器不但可以作用于for循环，还可以被next()函数不断调用并返回下一个值，直到最后抛出StopIteration错误表示无法继续返回下一个值了。
+# 可以被next()函数调用并不断返回下一个值的对象称为迭代器：Iterator。
+from collections.abc import Iterator  #3.8版本开始不支持from collection；支持collection.abc
+print(isinstance((x for x in range(10)), Iterator))
+print(isinstance([], Iterator))
+print(isinstance({}, Iterator))
+print(isinstance('abcd', Iterator))
+
+# 生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator。
+# 把list、dict、str等Iterable变成Iterator可以使用iter()函数
+print(isinstance(iter([]), Iterator))
+print(isinstance(iter('abc'), Iterator))
+
+# 凡是可以for循环的，都是Iterable
+# 凡是可以next()的，都是Iterator，它们表示一个惰性计算的序列
+# Iterator甚至可以表示一个无限大的数据流，例如全体自然数。而使用list是永远不可能存储全体自然数的
+# 集合数据类型如list、dict、str等是Iterable但不是Iterator，不过可以通过iter()函数获得一个Iterator对象
+
+# Python的for循环本质上就是通过不断调用next()函数实现的，例如：
+for x in [1, 2, 3, 4, 5]:
+    pass
+# 实际上完全等价于：
+# 首先获得Iterator对象:
+it = iter([1, 2, 3, 4, 5])
+# 循环:
+while True:
+    try:
+        # 获得下一个值:
+        x = next(it)
+    except StopIteration:
+        # 遇到StopIteration就退出循环
+        break
+
+# iter list:
+print('for x in [1, 2, 3, 4, 5]:')
+for x in [1, 2, 3, 4, 5]:
+    print(x)
+
+print('for x in iter([1, 2, 3, 4, 5]):')
+for x in iter([1, 2, 3, 4, 5]):
+    print(x)
+
+print('next():')
+it = iter([1, 2, 3, 4, 5])
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
+d = {'a': 1, 'b': 2, 'c': 3}
+
+# iter each key:
+print('iter key:', d)
+for k in d.keys():
+    print('key:', k)
+
+# iter each value:
+print('iter value:', d)
+for v in d.values():
+    print('value:', v)
+
+# iter both key and value:
+print('iter item:', d)
+for k, v in d.items():
+    print('item:', k, v)
+
+# iter list with index:
+print('iter enumerate([\'A\', \'B\', \'C\']')
+for i, value in enumerate(['A', 'B', 'C']):
+    print(i, value)
+
+# iter complex list:
+print('iter [(1, 1), (2, 4), (3, 9)]:')
+for x, y in [(1, 1), (2, 4), (3, 9)]:
+    print(x, y)
